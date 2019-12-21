40fcd7f982e6b410649b6f4310d86803bbd077f8
何召顺 <373693643@qq.com>
2019-12-21 14:19:50 +0800

    自定义组件props

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
-import { Image,
-        StyleSheet, 
-        View
-} from 'react-native';
-import { create } from 'react-test-renderer';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-        <View style={styles.page}>
-            <Image source={pic} style={styles.img} />
-        </View>
+      <View style={{alignItems: 'center', marginTop: 50}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-    page: {
-        width: '100%',
-        height: '100%',
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    img: {
-        width: 193,
-        height: 110
-    } 
-})
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
