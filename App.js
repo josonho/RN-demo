e464dda1413fdd448cc2a407a9e5de0ac320840c
何召顺 <373693643@qq.com>
2020-01-07 16:18:28 +0800

    处理触摸事件--显示一个简单的按钮

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Button, Alert } from 'react-native';
 
 export default class PizzaTranslator extends Component {
-    state = {
-        text: ''
-    }
-
     render() {
-        return (
-            <View style={{padding: 10}}>
-                <TextInput
-                    style={{height: 40}}
-                    placeholder='Type here to translate'
-                    onChangeText={ (text) => this.setState({text}) }
-                />
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
-            </View>
+        return ( 
+            <Button
+                onPress={() => {
+                    Alert.alert("你点击了按钮！");
+                }}
+                title="点我！"
+            />
         )
     }
 }
