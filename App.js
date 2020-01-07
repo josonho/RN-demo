961c6e02db5d84922ffc499320f11f331874037c
何召顺 <373693643@qq.com>
2020-01-07 15:42:48 +0800

    使用Flexbox布局--Flex

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
     render() {
         return (
-            // 试试去掉父View中的`flex: 1`。
-            // 则父View不再具有尺寸，因此子组件也无法再撑开。
-            // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, height: 50, backgroundColor: 'powderblue'}}/>
-                <View style={{flex: 2, height: 100, backgroundColor: 'skyblue'}}/>
-                <View style={{flex: 3, height: 150, backgroundColor: 'steelblue'}}/>
+            // 尝试把'flexDirection'改为'column'看看
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
             </View>
         )
     }
