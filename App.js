6abe74e9b9d10759169653845e324d8a839d4193
何召顺 <373693643@qq.com>
2020-01-07 15:47:14 +0800

    使用Flexbox布局--AlignItems

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ export default class FlexDirectionBasics extends Component {
     render() {
         return (
             // 尝试把'flexDirection'改为'column'看看
-            <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{
+                flex: 1, 
+                flexDirection: 'column', 
+                justifyContent: 'center',
+                alignItems: 'stretch'
+            }}>
                 <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
+                <View style={{height: 50, backgroundColor: 'skyblue'}}/>
+                <View style={{height: 100, backgroundColor: 'steelblue'}}/>
             </View>
         )
     }
